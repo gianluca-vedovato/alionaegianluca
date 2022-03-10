dd5ae76e5a877be772ee69806aece7debddc24af
Gianluca Vedovato <vedovatogianluca@gmail.com>
2022-03-10 21:40:12 +0100

    text md

--- alionaegianluca.it/app/dataset/messages/md/intro.js
+++ alionaegianluca.it/app/dataset/messages/md/intro.js
@@ -1,7 +1,7 @@
 export default (isSingular) => ({
   greeting: 'Ciao',
   main: {
-    full: `suntem bucuroși să vă invităm la nunta noastră care va avea loc sâmbătă, 25 iunie 2022, la Biserica <strong> San Giorgio </strong> din <strong> Chirignago </strong> (Veneția), la <strong> ora 15.30 </strong>.`,
-    partial: `suntem bucuroși să vă invităm la nunta noastră care va avea loc sâmbătă, 25 iunie 2022, la Biserica <strong> San Giorgio </strong> din <strong> Chirignago </strong> (Veneția), la <strong> ora 15.30 </strong>.`
+    full: `suntem bucuroși să vă invităm la nunta noastră care va avea loc sâmbătă, 25 iunie 2022, la Biserica <strong> San Giorgio </strong> din <strong> Chirignago </strong>, la <strong> ora 15.30 </strong>.`,
+    partial: `suntem bucuroși să vă invităm la nunta noastră care va avea loc sâmbătă, 25 iunie 2022, la Biserica <strong> San Giorgio </strong> din <strong> Chirignago </strong>, la <strong> ora 15.30 </strong>.`
   }
 })
--- alionaegianluca.it/app/dataset/messages/md/invitation.js
+++ alionaegianluca.it/app/dataset/messages/md/invitation.js
@@ -1,16 +1,16 @@
 export default (isSingular) => ({
   full: {
-    title: "După ceremonie v-om merge la Segnavento (Zelarino) pentru a sărbători.După ceremonie, ne vom muta la ferma Al Segnavento din Zelarino (VE) pentru recepție.",
+    title: "După ceremonie v-om merge la Segnavento pentru a sărbători.",
     text: {
-      part1: 'Va fi un aperitiv sh o masa de seara cu producte locale.',
+      part1: '',
       part2: 'Apoi va fi muzică și un open bar 😃'
     }
   },
   partial: {
-    title: `Ne face plăcere să vă avem ca oaspeți pentru seara care va avea loc la ferma Al Segnavento din Zelarino (Veneția).`,
+    title: `Ne face plăcere să vă avem ca oaspeți pentru seara care va avea loc la ferma Al Segnavento.`,
     text: `Am fi bucuroși să vă primim pentru tăierea tortului de nuntă care va avea loc la ora 21.30. După aceea va fi muzică și un open bar, poți dansa desculț, te simți confortabil😃`,
   },
   clickHere: 'Anunțați-ne ',
-  allergens: ' dacă aveți alergii, pentru a vă  putea oferi un serviciu adecvat.',
+  allergens: ' dacă aveți alergii la mancare, pentru a vă  putea oferi un serviciu adecvat.',
   showOnMap: 'Arată-mi pe hartă',
 })

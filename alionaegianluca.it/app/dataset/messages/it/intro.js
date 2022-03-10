4ccb558ebc879265a658ffb4043aa651fd2bf920
Gianluca Vedovato <vedovatogianluca@gmail.com>
2022-03-10 12:17:24 +0100

    text

--- alionaegianluca.it/app/dataset/messages/it/intro.js
+++ alionaegianluca.it/app/dataset/messages/it/intro.js
@@ -2,6 +2,6 @@ export default (isSingular) => ({
   greeting: 'Ciao',
   main: {
     full: `abbiamo il piacere di ${isSingular ? 'invitarti' : 'invitarvi'} al nostro matrimonio che si terrà <strong>sabato 25 giugno 2022</strong> presso la Chiesa di <strong>San Giorgio</strong> di <strong>Chirignago</strong> (Venezia) alle ore <strong>15.30</strong>.`,
-    partial: `abbiamo il piacere di ${isSingular ? 'invitarti' : 'invitarvi'} al nostro matrimonio che si terrà il <strong>25 Giugno 2022</strong>. La cerimonia, per chi ha piacere, avrà luogo presso la Chiesa di<strong> San Giorgio</strong> di <strong>Chirignago</strong> (VE) alle ore <strong>15.30</strong>.`
+    partial: `abbiamo il piacere di ${isSingular ? 'invitarti' : 'invitarvi'} al nostro matrimonio che si terrà il <strong>25 Giugno 2022</strong>. <br />La cerimonia, per chi ha piacere, avrà luogo presso la Chiesa di<strong> San Giorgio</strong> di <strong>Chirignago</strong> (VE) alle ore <strong>15.30</strong>.`
   }
 })
